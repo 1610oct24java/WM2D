7ef33d6c083f5e11c9055da1c84492947f67f78d
Willie <Willie@WhoopieJensen>
2016-12-01 14:42:51 -0500

    modified folder structure
    pom.xml
    hibernateUtil
    random com folder

---
+++ MyFridge/src/main/java/com/revature/util/HibernateUtil.java
@@ -0,0 +1,37 @@
+package com.revature.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil
+{
+	private SessionFactory sessionFactory;
+	public SessionFactory getSessionFactory()
+	{
+		if(sessionFactory==null)
+		{
+			//make a new one
+			Configuration conf = new Configuration().configure();
+			ServiceRegistry serviceRegistry =
+					new StandardServiceRegistryBuilder()
+						.applySettings(conf.getProperties())
+							.build();
+			//Build a session factory from the service registry
+			sessionFactory = conf.buildSessionFactory(serviceRegistry);
+		}
+		return sessionFactory;
+	}
+	public Session getSession()
+	{
+		return this.getSessionFactory().openSession();
+	}
+	@Override
+	protected void finalize() throws Throwable {
+		sessionFactory.close();
+		super.finalize();
+	}
+	
+}
