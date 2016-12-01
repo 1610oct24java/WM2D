aa47e2e21170c731a735c9798eedce506c7c4587
Douglas Shoemaker <dougshmkr@gmail.com>
2016-12-01 13:30:40 -0500

    Adding Java src folders

---
+++ MyFridge/src/main/java/com/revature/test/Testing.java
@@ -0,0 +1,15 @@
+package com.revature.test;
+
+
+public class Testing {
+	
+	public Testing() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
